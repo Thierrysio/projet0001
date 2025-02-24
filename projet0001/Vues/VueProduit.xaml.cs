a6bd0d73b482d2490ff27b5dd4de7af4c29b87e4
thierrysio <thierry28220@icloud.com>
2025-02-24 11:09:46 +0100

    collection

--- projet0001/Vues/VueProduit.xaml.cs
+++ projet0001/Vues/VueProduit.xaml.cs
@@ -1,29 +1,40 @@
 using projet0001.Modeles;
+using System.Collections.ObjectModel;
 
 namespace projet0001.Vues;
 
 public partial class VueProduit : ContentPage
 {
-	public VueProduit()
-	{
-		InitializeComponent();
-		Produit P1 = new Produit("Disney","riri");
+    // Déclaration publique pour le binding
+    public ObservableCollection<Produit> Items { get; } = new ObservableCollection<Produit>();
+
+    public VueProduit()
+    {
+        InitializeComponent();
+        BindingContext = this;
+
+        // Création des produits
+        Produit P1 = new Produit("Disney", "riri");
+        Produit P2 = new Produit("Disney", "fifi");
+        Produit P3 = new Produit("Disney", "loulou");
+
+        Items.Add(P1);
+        Items.Add(P2);
+        Items.Add(P3);
 
         l1.Text = P1.ConcatNomPrenom();
-	}
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-		string res = E1.Text;
-		l1.Text = res;
+        string res = E1.Text;
+        l1.Text = res;
     }
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
-        string nb1 = N1.Text;
-        string nb2 = N2.Text;
-        int nbre1 = int.Parse(nb1);
-        int nbre2 = int.Parse(nb2);
+        int nbre1 = int.Parse(N1.Text);
+        int nbre2 = int.Parse(N2.Text);
         int resultat = nbre1 + nbre2;
         LResultat.Text = resultat.ToString();
     }
