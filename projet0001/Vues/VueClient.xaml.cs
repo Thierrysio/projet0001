2032f44ded24f1af54db39c3f557d2b136c60013
thierrysio <thierry28220@icloud.com>
2025-02-24 11:40:41 +0100

    images

--- projet0001/Vues/VueClient.xaml.cs
+++ projet0001/Vues/VueClient.xaml.cs
@@ -1,9 +1,18 @@
+using projet0001.Modeles;
+using System.Collections.ObjectModel;
+
 namespace projet0001.Vues;
 
 public partial class VueClient : ContentPage
 {
-	public VueClient()
+    public ObservableCollection<Client> Clients { get; } = new ObservableCollection<Client>();
+
+    public VueClient()
 	{
 		InitializeComponent();
-	}
+		BindingContext = this;
+		Clients.Add(new Client("TOTO", "https://c8.alamy.com/compfr/r1adk8/etudiant-diplome-teen-girl-r1adk8.jpg"));
+        Clients.Add(new Client("TITI", "https://c8.alamy.com/compfr/r1adk8/etudiant-diplome-teen-girl-r1adk8.jpg"));
+        Clients.Add(new Client("TUTU", "https://c8.alamy.com/compfr/r1adk8/etudiant-diplome-teen-girl-r1adk8.jpg"));
+    }
 }
