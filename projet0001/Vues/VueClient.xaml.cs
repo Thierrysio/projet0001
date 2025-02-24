a8f244bd374d03b264bdaa56bee5422d097b2144
thierrysio <thierry28220@icloud.com>
2025-02-24 13:54:37 +0100

    ok

--- projet0001/Vues/VueClient.xaml.cs
+++ projet0001/Vues/VueClient.xaml.cs
@@ -15,4 +15,16 @@ public partial class VueClient : ContentPage
         Clients.Add(new Client("TITI", "https://c8.alamy.com/compfr/r1adk8/etudiant-diplome-teen-girl-r1adk8.jpg"));
         Clients.Add(new Client("TUTU", "https://c8.alamy.com/compfr/r1adk8/etudiant-diplome-teen-girl-r1adk8.jpg"));
     }
+
+
+
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // Récupère le premier élément sélectionné
+        var selectedClient = e.CurrentSelection.FirstOrDefault() as Client;
+        if (selectedClient != null)
+        {
+            // Effectuez des actions avec l'élément sélectionné
+        }
+    }
 }
